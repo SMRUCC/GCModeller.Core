96386c47e2f9c4bc166c8b5b17b10cb2d0f0b2bb
xieguigang <xie.guigang@gmail.com>
2016-04-19 21:02:37 +0800

    reconstruct uniprot module

---
+++ Bio.Assembly/Assembly/Uniprot/Web/Entry.vb
@@ -0,0 +1,17 @@
+﻿
+Namespace Assembly.Uniprot.Web
+
+    Public Class Entry
+        Public Property Entry As ComponentModel.KeyValuePair
+        Public Property EntryName As String
+        Public Property StatusReviewed As Boolean
+        Public Property ProteinNames As String
+        Public Property GeneNames As String
+        Public Property Organism As String
+        Public Property Length As String
+
+        Public Overrides Function ToString() As String
+            Return Entry.ToString
+        End Function
+    End Class
+End Namespace
--- Bio.Resources/Resources.vb
+++ Bio.Resources/Resources.vb
@@ -1,9 +1,16 @@
 ﻿Imports System.ComponentModel.Composition
 Imports System.Resources
 
+''' <summary>
+''' External resource data EXPORT API
+''' </summary>
 <Export(GetType(ResourceManager))>
 Public Module Resources
 
+    ''' <summary>
+    ''' Export the resource data to the assembly LANS.SystemsBiology.Assembly.Components_v2.0_33.0.0.0__89845dcd8080cc91
+    ''' </summary>
+    ''' <returns></returns>
     <Export(GetType(ResourceManager))>
     Public ReadOnly Property Resources As ResourceManager
         Get
