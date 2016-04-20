ba2be7aee23280eac0178debaeab7bde4b7e3223
xieguigang <xie.guigang@gmail.com>
2016-04-20 22:06:20 +0800

    add a patterns interface

---
+++ Bio.Assembly/SequenceModel/Patterns/IPatterns.vb
@@ -0,0 +1,10 @@
+﻿Namespace SequenceModel
+
+    Public Interface IPatternProvider
+        Function PWM() As IEnumerable(Of IPatternSite)
+    End Interface
+
+    Public Interface IPatternSite
+        Default ReadOnly Property Probability(c As Char) As Double
+    End Interface
+End Namespace
