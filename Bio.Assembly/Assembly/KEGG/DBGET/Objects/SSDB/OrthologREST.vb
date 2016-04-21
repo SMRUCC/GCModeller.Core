a24a3cd7b519a4ecb26b405bb9fb2cabbf90fb93
xieguigang <xie.guigang@gmail.com>
2016-04-21 17:12:07 +0800

    code readable improvements

--- Bio.Assembly/Assembly/KEGG/DBGET/Objects/SSDB/OrthologREST.vb
+++ Bio.Assembly/Assembly/KEGG/DBGET/Objects/SSDB/OrthologREST.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Text.RegularExpressions
 Imports System.Xml.Serialization
+Imports LANS.SystemsBiology.Assembly.KEGG.WebServices
+Imports LANS.SystemsBiology.SequenceModel
+Imports Microsoft.VisualBasic.Language
 
 Namespace Assembly.KEGG.DBGET.bGetObject.SSDB
 
@@ -10,35 +13,39 @@ Namespace Assembly.KEGG.DBGET.bGetObject.SSDB
     ''' 在直系同源的数据被下载下来之后，这个对象会被直接保存为Xml文档
     ''' </remarks>
     <XmlType("Ortholog")>
-    Public Class OrthologREST
+    Public Class OrthologREST : Inherits ClassObject
 
         Public Const URL As String = "http://www.kegg.jp/ssdb-bin/ssdb_best?org_gene={0}:{1}"
 
         <XmlAttribute> Public Property KEGG_ID As String
         <XmlAttribute> Public Property Definition As String
         <XmlElement> Public Property Sequence As String
-
+        ''' <summary>
+        ''' 直系同源的蛋白质比对结果
+        ''' </summary>
+        ''' <returns></returns>
         <XmlElement> Public Property Orthologs As SShit()
 
         Public Const SEPRATOR As String = "------------------------------------------------------------------ -------------------------------------------------------------"
 
-        '<INPUT TYPE="checkbox" NAME="ckid" VALUE="xca:xccb100_1230" CHECKED><A HREF="http://www.kegg.jp/dbget-bin/www_bget?xca:xccb100_1230" TARGET="_blank">xca:xccb100_1230</A> type IV pilus response regulator PilH  <A HREF="http://www.kegg.jp/dbget-bin/www_bget?ko:K02658"  TARGET="_blank">K02658</a>     120      771 (  378)     182    1.000    120     &lt;-&gt; <a href='/ssdb-bin/ssdb_ortholog_view?org_gene=xcb:XC_1184&org=xca&threshold=&type=' target=_ortholog>66</a>
+        ' <INPUT TYPE="checkbox" NAME="ckid" VALUE="xca:xccb100_1230" CHECKED><A HREF="http://www.kegg.jp/dbget-bin/www_bget?xca:xccb100_1230" TARGET="_blank">xca:xccb100_1230</A> type IV pilus response regulator PilH  <A HREF="http://www.kegg.jp/dbget-bin/www_bget?ko:K02658"  TARGET="_blank">K02658</a>     120      771 (  378)     182    1.000    120     &lt;-&gt; <a href='/ssdb-bin/ssdb_ortholog_view?org_gene=xcb:XC_1184&org=xca&threshold=&type=' target=_ortholog>66</a>
         Public Const REGEX_ORTHO_ITEM As String = "<INPUT TYPE=""checkbox"" .+? target=_ortholog>\d+</a>"
 
-        Public Shared Function Download(Entry As KEGG.WebServices.QueryEntry) As OrthologREST
-            Dim pageContent As String = String.Format(KEGG.DBGET.bGetObject.SSDB.OrthologREST.URL, Entry.SpeciesId, Entry.LocusId).GET
-            Dim Ortholog As OrthologREST = New OrthologREST
-            Dim Tokens = Strings.Split(pageContent, KEGG.DBGET.bGetObject.SSDB.OrthologREST.SEPRATOR)
-            Dim Items As String() = (From strData As Match
-                                     In Regex.Matches(Tokens.Last, REGEX_ORTHO_ITEM, RegexOptions.IgnoreCase + RegexOptions.Singleline)
-                                     Select strData.Value).ToArray
-            Dim LQuery = (From strData As String
-                          In Items
-                          Select KEGG.DBGET.bGetObject.SSDB.SShit.CreateObject(strData)).ToArray
-            Ortholog.Orthologs = LQuery
-            Ortholog.KEGG_ID = Entry.ToString
+        Public Overrides Function ToString() As String
+            Return Definition
+        End Function
 
-            Dim Fa = KEGG.WebServices.WebRequest.FetchSeq(Entry)
+        Public Shared Function Download(locusTag As QueryEntry) As OrthologREST
+            Dim html As String = String.Format(URL, locusTag.SpeciesId, locusTag.LocusId).GET
+            Dim tokens = Strings.Split(html, SEPRATOR)
+            Dim hits As String() = Regex.Matches(tokens.Last, REGEX_ORTHO_ITEM, RegexICSng).ToArray
+
+            Dim Ortholog As New OrthologREST With {
+                .Orthologs = LinqAPI.Exec(Of String, SShit)(hits) <= Function(s) SShit.CreateObject(s),
+                .KEGG_ID = locusTag.ToString
+            }
+
+            Dim Fa As FASTA.FastaToken = WebRequest.FetchSeq(locusTag)
 
             If Not Fa Is Nothing Then
                 Ortholog.Definition = Fa.Title
