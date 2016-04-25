a6c890b61f6f10a56db6aa0c195e20c999a7a5ca
ameth <amethyst.asuka@gcmodeller.org>
2016-04-25 11:36:51 +0800

    database installer

--- Bio.Repository/Genbank.vb
+++ Bio.Repository/Genbank.vb
@@ -1,5 +1,43 @@
-﻿Imports Microsoft.VisualBasic.Language
+﻿Imports LANS.SystemsBiology.Assembly.NCBI.GenBank
+Imports Microsoft.VisualBasic.DocumentFormat.Csv
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Serialization
+
+Public Module Installer
+
+    ''' <summary>
+    ''' 这个函数主要是进行创建数据库的索引文件
+    ''' </summary>
+    ''' <param name="DIR">Extract location of file: all.gbk.tar.gz from NCBI FTP website.</param>
+    ''' <returns></returns>
+    Public Function Install(DIR As String) As Boolean
+        Dim index As String = $"{DIR}/ncbi_genbank.csv"
+
+        Call "".SaveTo(index)  ' 清除原始文件的所有数据，重新创建索引文件
+
+        Using DbWriter As New DocumentStream.Linq.WriteStream(Of GenbankIndex)(index)
+
+            For Each table As String In ls - l - lsDIR - r <= DIR
+                For Each gbk As String In ls - l - r - wildcards("*.gbk", "*.gb") <= table
+                    For Each gb As GBFF.File In GBFF.File.LoadDatabase(gbk)  ' 读取数据库文件
+
+                        Dim idx As New GenbankIndex With {
+                            .AccId = gb.Locus.AccessionID,
+                            .definition = gb.Definition.Value,
+                            .DIR = table.BaseName,
+                            .genome = gb.Source.SpeciesName
+                        }
+                        Call DbWriter.Flush(idx)   ' 将对象写入内存缓存，进入队列等待回写入文件系统
+
+                    Next
+                Next
+            Next
+        End Using
+
+        Return True
+    End Function
+End Module
 
 Public Class Genbank : Inherits ClassObject
 
@@ -8,21 +46,13 @@ Public Class Genbank : Inherits ClassObject
     Sub New(DIR As String)
         Me.DIR = DIR
     End Sub
-
-    ''' <summary>
-    ''' 创建索引文件
-    ''' </summary>
-    ''' <param name="DIR"></param>
-    ''' <returns></returns>
-    Public Shared Function Install(DIR As String) As Integer
-
-    End Function
 End Class
 
 Public Class GenbankIndex
     Public Property DIR As String
     Public Property genome As String
     Public Property AccId As String
+    Public Property definition As String
 
     Public Overrides Function ToString() As String
         Return Me.GetJson
