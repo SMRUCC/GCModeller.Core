67a9cc878a1f2ec16ef5fbc99265d3a7b1d3b23f
明日香 <amethyst.asuka@gcmodeller.org>
2016-05-30 17:09:03 +0800

    improvements on the context interface

--- Bio.Assembly/ContextModel/Context.vb
+++ Bio.Assembly/ContextModel/Context.vb
@@ -1,28 +1,39 @@
 ﻿Imports LANS.SystemsBiology.ComponentModel
 Imports LANS.SystemsBiology.ComponentModel.Loci
+Imports LANS.SystemsBiology.SequenceModel.NucleotideModels
 Imports Microsoft.VisualBasic.Serialization
 
 Namespace ContextModel
 
-    Public Structure Context(Of T As IGeneBrief)
+    Public Structure Context
 
-        Public ReadOnly Feature As T
+        Public ReadOnly Feature As NucleotideLocation
         Public ReadOnly Upstream As NucleotideLocation
         Public ReadOnly Downstream As NucleotideLocation
         Public ReadOnly Antisense As NucleotideLocation
+        Public ReadOnly Tag As String
 
-        Sub New(g As T, dist As Integer)
-            Feature = g
+        Sub New(g As IGeneBrief, dist As Integer)
+            Call Me.New(g.Location, dist, g.ToString)
+        End Sub
 
-            If g.Location.Strand = Strands.Forward Then
-                Upstream = New NucleotideLocation(g.Location.Left - dist, g.Location.Left, Strands.Forward)
-                Downstream = New NucleotideLocation(g.Location.Right, g.Location.Right + dist, Strands.Forward)
-                Antisense = New NucleotideLocation(g.Location.Left, g.Location.Right, Strands.Reverse)
+        Sub New(loci As NucleotideLocation, dist As Integer, Optional tag As String = Nothing)
+            Feature = loci
+            tag = NotEmpty(tag, loci.ToString)
+
+            If loci.Strand = Strands.Forward Then
+                Upstream = New NucleotideLocation(loci.Left - dist, loci.Left, Strands.Forward)
+                Downstream = New NucleotideLocation(loci.Right, loci.Right + dist, Strands.Forward)
+                Antisense = New NucleotideLocation(loci.Left, loci.Right, Strands.Reverse)
             Else
-                Upstream = New NucleotideLocation(g.Location.Right, g.Location.Right + dist, Strands.Reverse)
-                Downstream = New NucleotideLocation(g.Location.Left - dist, g.Location.Left, Strands.Reverse)
-                Antisense = New NucleotideLocation(g.Location.Left, g.Location.Right, Strands.Forward)
+                Upstream = New NucleotideLocation(loci.Right, loci.Right + dist, Strands.Reverse)
+                Downstream = New NucleotideLocation(loci.Left - dist, loci.Left, Strands.Reverse)
+                Antisense = New NucleotideLocation(loci.Left, loci.Right, Strands.Forward)
             End If
+        End Sub
+
+        Sub New(g As Contig, dist As Integer)
+            Call Me.New(g.MappingLocation, dist, g.ToString)
         End Sub
 
         Public Function GetRelation(loci As NucleotideLocation, stranded As Boolean) As SegmentRelationships
@@ -37,7 +48,7 @@ Namespace ContextModel
             Dim rel As SegmentRelationships = __getRel(loci)
 
             If rel = SegmentRelationships.Blank Then
-                If loci.Strand <> Feature.Location.Strand Then
+                If loci.Strand <> Feature.Strand Then
                     If Antisense.IsInside(loci) Then
                         Return SegmentRelationships.InnerAntiSense
                     End If
@@ -52,20 +63,20 @@ Namespace ContextModel
                 Return SegmentRelationships.UpStream
             ElseIf Downstream.IsInside(loci) Then
                 Return SegmentRelationships.DownStream
-            ElseIf Feature.Location.Equals(loci, 1) Then
+            ElseIf Feature.Equals(loci, 1) Then
                 Return SegmentRelationships.Equals
-            ElseIf Feature.Location.IsInside(loci) Then
+            ElseIf Feature.IsInside(loci) Then
                 Return SegmentRelationships.Inside
             Else
-                If Feature.Location.IsInside(loci.Left) AndAlso Upstream.IsInside(loci.Right) Then
+                If Feature.IsInside(loci.Left) AndAlso Upstream.IsInside(loci.Right) Then
                     Return SegmentRelationships.UpStreamOverlap
-                ElseIf Feature.Location.IsInside(loci.Right) AndAlso Upstream.IsInside(loci.Left) Then
+                ElseIf Feature.IsInside(loci.Right) AndAlso Upstream.IsInside(loci.Left) Then
                     Return SegmentRelationships.UpStreamOverlap
-                ElseIf Feature.Location.IsInside(loci.Left) AndAlso Downstream.IsInside(loci.Right) Then
+                ElseIf Feature.IsInside(loci.Left) AndAlso Downstream.IsInside(loci.Right) Then
                     Return SegmentRelationships.DownStreamOverlap
-                ElseIf Feature.Location.IsInside(loci.Right) AndAlso Downstream.IsInside(loci.Left) Then
+                ElseIf Feature.IsInside(loci.Right) AndAlso Downstream.IsInside(loci.Left) Then
                     Return SegmentRelationships.DownStreamOverlap
-                ElseIf loci.IsInside(Feature.Location) Then
+                ElseIf loci.IsInside(Feature) Then
                     Return SegmentRelationships.Cover
                 Else
                     Return SegmentRelationships.Blank
@@ -74,7 +85,7 @@ Namespace ContextModel
         End Function
 
         Private Function __relStranede(loci As NucleotideLocation) As SegmentRelationships
-            If loci.Strand <> Feature.Location.Strand Then  ' 不在同一条链之上
+            If loci.Strand <> Feature.Strand Then  ' 不在同一条链之上
                 If Antisense.IsInside(loci) Then
                     Return SegmentRelationships.InnerAntiSense
                 Else
@@ -86,7 +97,7 @@ Namespace ContextModel
         End Function
 
         Public Overrides Function ToString() As String
-            Return Me.GetJson
+            Return Tag
         End Function
     End Structure
 End Namespace
