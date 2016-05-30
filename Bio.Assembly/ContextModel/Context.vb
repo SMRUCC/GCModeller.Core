79a87a2923358768d534c90232600c7495bdf675
明日香 <amethyst.asuka@gcmodeller.org>
2016-05-30 17:02:29 +0800

    works on a more accurate context model

--- Bio.Assembly/ContextModel/Context.vb
+++ Bio.Assembly/ContextModel/Context.vb
@@ -34,12 +34,54 @@ Namespace ContextModel
         End Function
 
         Private Function __relUnstrand(loci As NucleotideLocation) As SegmentRelationships
+            Dim rel As SegmentRelationships = __getRel(loci)
 
+            If rel = SegmentRelationships.Blank Then
+                If loci.Strand <> Feature.Location.Strand Then
+                    If Antisense.IsInside(loci) Then
+                        Return SegmentRelationships.InnerAntiSense
+                    End If
+                End If
+            End If
+
+            Return rel
+        End Function
+
+        Private Function __getRel(loci As NucleotideLocation) As SegmentRelationships
+            If Upstream.IsInside(loci) Then
+                Return SegmentRelationships.UpStream
+            ElseIf Downstream.IsInside(loci) Then
+                Return SegmentRelationships.DownStream
+            ElseIf Feature.Location.Equals(loci, 1) Then
+                Return SegmentRelationships.Equals
+            ElseIf Feature.Location.IsInside(loci) Then
+                Return SegmentRelationships.Inside
+            Else
+                If Feature.Location.IsInside(loci.Left) AndAlso Upstream.IsInside(loci.Right) Then
+                    Return SegmentRelationships.UpStreamOverlap
+                ElseIf Feature.Location.IsInside(loci.Right) AndAlso Upstream.IsInside(loci.Left) Then
+                    Return SegmentRelationships.UpStreamOverlap
+                ElseIf Feature.Location.IsInside(loci.Left) AndAlso Downstream.IsInside(loci.Right) Then
+                    Return SegmentRelationships.DownStreamOverlap
+                ElseIf Feature.Location.IsInside(loci.Right) AndAlso Downstream.IsInside(loci.Left) Then
+                    Return SegmentRelationships.DownStreamOverlap
+                ElseIf loci.IsInside(Feature.Location) Then
+                    Return SegmentRelationships.Cover
+                Else
+                    Return SegmentRelationships.Blank
+                End If
+            End If
         End Function
 
         Private Function __relStranede(loci As NucleotideLocation) As SegmentRelationships
-            If loci.Strand <> Feature.Location.Strand Then
-
+            If loci.Strand <> Feature.Location.Strand Then  ' 不在同一条链之上
+                If Antisense.IsInside(loci) Then
+                    Return SegmentRelationships.InnerAntiSense
+                Else
+                    Return SegmentRelationships.Blank
+                End If
+            Else
+                Return __getRel(loci)
             End If
         End Function
 
