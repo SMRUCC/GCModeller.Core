8b1784c1bcfe19c224e1fe802c9677157aeb3b28
xieguigang <xie.guigang@gmail.com>
2016-11-17 22:53:52 +0800

    scored site model

--- Bio.Assembly/ComponentModel/Loci.Models/MotifSite.vb
+++ Bio.Assembly/ComponentModel/Loci.Models/MotifSite.vb
@@ -6,4 +6,12 @@
         Property Name As String
         Property Site As Location
     End Interface
+
+    ''' <summary>
+    ''' This motif site have the scoring calculation value
+    ''' </summary>
+    Public Interface IMotifScoredSite : Inherits IMotifSite
+
+        Property Score As Double
+    End Interface
 End Namespace
