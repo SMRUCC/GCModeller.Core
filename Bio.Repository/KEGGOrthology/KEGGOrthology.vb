53386bc8017acf9d196b27aa4908f2bff82fe12a
xieguigang <xie.guigang@gmail.com>
2017-01-07 13:42:28 +0800

    build ko00001 index

--- Bio.Repository/KEGGOrthology/KEGGOrthology.vb
+++ Bio.Repository/KEGGOrthology/KEGGOrthology.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.Data.csv
+Imports Microsoft.VisualBasic.Data.csv.DocumentStream.Linq
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports SMRUCC.genomics.Assembly.KEGG.DBGET.bGetObject.SSDB
@@ -15,9 +16,13 @@ Public Class KEGGOrthology
 
     Dim locus2KO As New Dictionary(Of String, String())
 
+    Private Function __ko_genes_index() As String
+        Return (Repository & "/ko00001_genes_index.csv")
+    End Function
+
     Private Sub __locus2KO()
-        Dim package As IEnumerable(Of KO_gene) = (Repository & "/KO_genes_index.csv").LoadCsv(Of KO_gene)
-        Dim locus_tags = package.GroupBy(Function(kg) kg.gene)
+        Dim package As IEnumerable(Of KO_gene) = __ko_genes_index().LoadCsv(Of KO_gene)
+        Dim locus_tags = package.GroupBy(Function(kg) kg.sp_code & ":" & kg.gene)
 
         For Each gene As IGrouping(Of String, KO_gene) In locus_tags
             Dim k$() = gene _
@@ -27,6 +32,19 @@ Public Class KEGGOrthology
 
             Call locus2KO.Add(gene.Key, k)
         Next
+    End Sub
+
+    ''' <summary>
+    ''' 重新构建基因编号的索引
+    ''' </summary>
+    Public Sub BuildLocusIndex()
+        Dim source As IEnumerable(Of KO_gene()) = BuildLocus2KOIndex(Repository & "/ko00001/")
+
+        Using writer As New WriteStream(Of KO_gene)(__ko_genes_index)
+            For Each block As KO_gene() In source
+                Call writer.Flush(block)
+            Next
+        End Using
     End Sub
 
     Public Shared Iterator Function BuildLocus2KOIndex(DIR$) As IEnumerable(Of KO_gene())
@@ -60,7 +78,7 @@ Public Class KEGGOrthology
             locus2KO.ContainsKey(locus_tag), locus2KO(locus_tag), New String() {})
 
         For Each KO$ In KOlist
-            Dim xml$ = $"{Repository}/KO/{KO}.xml"
+            Dim xml$ = $"{Repository}/ko00001/{KO}.xml"
             Dim o As Orthology = xml.LoadXml(Of Orthology)
 
             Yield o
