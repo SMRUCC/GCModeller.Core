e24adb20b6073b9c3d537bdafdaf8c08144e7525
xieguigang <xie.guigang@gmail.com>
2017-01-19 01:26:05 +0800

    KEGG cut sequence to fasta API

---
+++ Bio.Assembly/Assembly/Uniprot/Web/ID_types.vb
@@ -0,0 +1,13 @@
+﻿Namespace Assembly.Uniprot.Web
+
+    Public Enum ID_types
+        ''' <summary>
+        ''' UniProtKB
+        ''' </summary>
+        ACC
+        ''' <summary>
+        ''' UniRef90
+        ''' </summary>
+        NF90
+    End Enum
+End Namespace
