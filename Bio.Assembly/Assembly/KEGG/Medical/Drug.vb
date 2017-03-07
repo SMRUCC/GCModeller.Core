4dfc3a588a8bd8ac135ad4e059f380bdb1978f5f
xieguigang <xie.guigang@gmail.com>
2017-03-07 21:00:24 +0800

    improvements on the dblink object

---
+++ Bio.Assembly/Assembly/KEGG/Medical/Drug.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports SMRUCC.genomics.ComponentModel.DBLinkBuilder
+
+Namespace Assembly.KEGG
+
+    Public Class Drug
+
+        Public Property Entry As String
+        Public Property Names As String()
+        Public Property Formula As String
+        Public Property Exact_Mass As Double
+        Public Property Mol_Weight As Double
+        Public Property Remarks As String()
+        Public Property Activity As String
+        Public Property DBLinks As DBLink()
+
+    End Class
+End Namespace
