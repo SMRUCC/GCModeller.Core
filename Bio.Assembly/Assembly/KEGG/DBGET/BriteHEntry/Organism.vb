f5d6d4b995af30918537523c52a84a4afd0f3ff3
xieguigang <xie.guigang@gmail.com>
2017-04-30 13:42:29 +0800

    kegg taxonomy tools

---
+++ Bio.Assembly/Assembly/KEGG/DBGET/BriteHEntry/Organism.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Language
+Imports SMRUCC.genomics.Metagenomics
+
+Namespace Assembly.KEGG.DBGET.BriteHEntry
+
+    ''' <summary>
+    ''' br08601
+    ''' </summary>
+    Public Module Organism
+
+        <Extension> Public Function Fill(organisms As htext) As Taxonomy()
+            Dim out As New List(Of Taxonomy)
+            Dim A$ = ""
+            Dim B$ = ""
+            Dim C$ = ""
+            Dim D$ = ""
+            Dim E$ = ""
+
+            For Each htext In organisms.Hierarchical.EnumerateEntries
+
+            Next
+
+            Return out
+        End Function
+    End Module
+End Namespace
