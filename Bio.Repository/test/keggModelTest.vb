722a58a11e183ec46d082ede13baf60b2527a018
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-06 02:48:10 +0800

    add test code

--- Bio.Repository/test/keggModelTest.vb
+++ Bio.Repository/test/keggModelTest.vb
@@ -1,7 +1,11 @@
-﻿Module keggModelTest
+﻿Imports SMRUCC.genomics.Data
+
+Module keggModelTest
 
     Sub Main()
+        Dim model As OrganismModel = "J:\XCC\xanthomonas\Xanthomonas citri subsp. citri MN12.Xml".LoadXml(Of OrganismModel)
 
+        Pause()
     End Sub
 
 End Module
