365e4016dc625b779d30f4c23f2e52af9a2db711
谢桂纲 <xie.guigang@gcmodeller.org>
2018-01-06 20:02:19 +0800

    add genome context feature scores description

---
+++ Bio.Assembly/ContextModel/Operon/FeatureScores.vb
@@ -0,0 +1,17 @@
+﻿Namespace ContextModel.Operon
+
+    ''' <summary>
+    ''' To evaluate the contribution of selected features in operon prediction, we have calculated 
+    ''' the numerical values of the features, And then used these values individually And in combination 
+    ''' to train a classifier. The features used in our study are
+    ''' 
+    ''' + (i)   the intergenic distance, 
+    ''' + (ii)  the conserved gene neighborhood, 
+    ''' + (iii) distances between adjacent genes' phylogenetic profiles, 
+    ''' + (iv)  the ratio between the lengths of two adjacent genes And 
+    ''' + (v)   frequencies Of specific DNA motifs in the intergenic regions.
+    ''' </summary>
+    Public Module FeatureScores
+
+    End Module
+End Namespace
