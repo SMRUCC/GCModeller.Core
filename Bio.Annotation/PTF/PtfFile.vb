738d0fde3cc5ff0277c4070ac4e3591df00f9422
xieguigang <xie.guigang@gcmodeller.org>
2020-06-26 03:49:57 +0800

    annotation data files

---
+++ Bio.Annotation/PTF/PtfFile.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Text
+
+Namespace Ptf
+
+    ''' <summary>
+    ''' the GCModeller protein annotation tabular format file.
+    ''' </summary>
+    Public Class PtfFile : Implements ISaveHandle
+
+        Public Property proteins As ProteinAnnotation()
+
+        Public Function Save(path As String, encoding As Encoding) As Boolean Implements ISaveHandle.Save
+            Using output As New StreamWriter(path.Open(doClear:=True), encoding) With {
+                .NewLine = ASCII.LF
+            }
+                Call Document.writeTabular(Me, output)
+            End Using
+
+            Return True
+        End Function
+
+        Public Function Save(path As String, Optional encoding As Encodings = Encodings.UTF8) As Boolean Implements ISaveHandle.Save
+            Return Save(path, encoding.CodePage)
+        End Function
+    End Class
+End Namespace
